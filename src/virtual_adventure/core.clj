3cd1be9bb883f9be8a8c32addcb9b0a96b4f6cd4
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2011-12-21 21:48:08 +0100

    Skeleton created with Leiningen

---
+++ src/virtual_adventure/core.clj
@@ -0,0 +1,1 @@
+(ns virtual-adventure.core)
---
+++ test/virtual_adventure/test/core.clj
@@ -0,0 +1,6 @@
+(ns virtual-adventure.test.core
+  (:use [virtual-adventure.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
