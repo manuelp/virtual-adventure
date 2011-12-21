72d8b43e08650693d276306cf3407c07c1cc7a3a
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2011-12-21 22:07:42 +0100

    Basic exploration and unconstrained moving

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject virtual-adventure "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject virtual-adventure "1.0.0"
+  :description "A simple textual adventure"
   :dependencies [[org.clojure/clojure "1.3.0"]])
--- src/virtual_adventure/core.clj
+++ src/virtual_adventure/core.clj
@@ -1,1 +1,19 @@
 (ns virtual-adventure.core)
+
+(def world {:start "It's an empty room, nothing interesting here.",
+            :north "This room is full of light and a celestial music.",
+            :south "This room is dark, cold and scary...",
+            :east "You are outside, there are high mountains far away.",
+            :west "Another room with an old wood table in the center."})
+
+(def where (ref :start))
+
+(defn explore
+  "Explore the current location where the character is."
+  []
+  (@where world))
+
+(defn move
+  "Move the character from the current location to the specified one."
+  [location]
+  (dosync (ref-set where location)))
